3ee555791b2af40b8854273eb641d0da8fec5da2
rannapareddy <rannapareddy@ITEM-S122053.emea.msad.sopra>
2022-08-30 12:13:13 +0530

    java project commit (first)

--- src/com/java/github/GithubDemo.java
+++ src/com/java/github/GithubDemo.java
@@ -3,8 +3,7 @@ package com.java.github;
 public class GithubDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Git hub demo");
 	}
 
 }
